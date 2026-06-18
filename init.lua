b109dbdf4a4787a173a971c95f5f4e462a84632b
DanielNov <danielnovak378@gmail.com>
2026-06-18 12:32:18 +0200

    add gitsigns, edit init.lua setting

--- init.lua
+++ init.lua
@@ -1,9 +1,18 @@
 require("config.lazy")
 require("config.lsp")
 
-vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.keymap.set("n", "-", "<cmd>Oil<CR>")
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
